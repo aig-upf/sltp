f2371bd960aee52ef3e13e79c117b8997fdf0bec
Blai Bonet <bonetblai@gmail.com>
2019-02-08 12:11:21 +0100

    update spanner.py in experiments-blai

--- experiments-blai/spanner.py
+++ experiments-blai/spanner.py
@@ -5,6 +5,22 @@ import sys
 from defaults import generate_experiment
 from sltp.util.misc import update_dict
 
+#
+# good abstraction:
+#   1. #loose nuts: loose(?n)
+#   2. #usable-spanners-being-carried: usable(?s) AND carry(m,?s)
+#   3. #usable-spanners-not-being-carried: usable(?s) AND -carry(m,?s)
+#   4. distance-to-usable-spanner-not-being-carried: dist(C1, R:C, C2), 
+#   5. distance-to-gate: ??
+#
+# move-towards-usable-spanner: f3 > 0, f4 > 0; dec(f4)
+# pick-spanner: f3 > 0, f4 = 0; dec(f3), inc(f2), inc*(f4)
+# move-towards-gate: f5 > 0; dec(f5)
+# tighten-nut: f1 > 0, f2 > 0; dec(f1), dec(f2)
+#
+# problem: abstraction may not be solvable by non-det
+#          effects on f1 and f2 by tighted-nut
+#
 
 def experiment(experiment_name=None):
     domain_dir = "spanner-small"
@@ -21,7 +37,7 @@ def experiment(experiment_name=None):
         num_states=523,
         max_concept_size=8,
         max_concept_grammar_iterations=3,
-        #distance_feature_max_complexity=8,
+        distance_feature_max_complexity=8,
         concept_generator=None,
         parameter_generator=add_domain_parameters,  # No goal concepts!
         feature_namer=feature_namer,)
