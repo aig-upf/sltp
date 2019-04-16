4eb88a06d66d79f8fc387ee1572182eb079cbffa
Guillem Frances <guillem.frances@gmail.com>
2019-04-16 13:24:35 +0200

    Cleanup build script

--- images/build.sh
+++ images/build.sh
@@ -24,23 +24,24 @@ trap cleanup EXIT
 
 pushd $WORK_DIR
 
+# Get Tarski
 git clone --depth 1 -b dev-0.2.0-symbols --single-branch git@github.com:aig-upf/tarski.git tarski
+
+# Get the FS planner
 git clone --depth 1 -b sltp-lite --single-branch git@github.com:aig-upf/fs-private.git fs
 cd fs && git submodule update --init && cd ..
 
-# git clone --depth 1 -b integrating-with-tarski --single-branch git@github.com:aig-upf/sltp.git sltp
+# Get current version of SLTP (from local directory)
 mkdir sltp
-git -C $SLTP_DIR checkout-index -a -f --prefix=`pwd`/sltp
+git -C $SLTP_DIR checkout-index -a -f --prefix=`pwd`/sltp/
 
-ls
-ls sltp
-
+# Build Docker image
 cp sltp/images/Dockerfile .
 docker build -t sltp .
 
 # Upload image to the amazon cluster
 docker save sltp | bzip2 | pv | ssh awscluster 'bunzip2 | docker load'
 
+# Cleanup tmp directory and go back to original directory
 cleanup
-
 popd
