aa51d55e3b2726d7231b055bd578c1644831b239
Guillem Frances <guillem.frances@gmail.com>
2019-04-18 11:43:22 +0200

    Minor improvements to the loading of experiments

--- experiments/run.py
+++ experiments/run.py
@@ -1,12 +1,35 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 import importlib
+import os
 import sys
 
 from sltp.util import console
 from sltp.util.bootstrap import setup_argparser
 
 from defaults import generate_experiment
+
+
+def import_from_file(filename):
+    """ Import a module from a given file path """
+    import importlib.util
+    spec = importlib.util.spec_from_file_location("imported", filename)
+    if spec is None:
+        report_and_exit('Could not import Python module "{}"'.format(filename))
+    mod = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(mod)
+    return mod
+
+
+def import_experiment_file(filename):
+    """ Import a module from a given file path """
+    if os.path.isfile(filename):
+        return import_from_file(filename)
+
+    try:
+        return importlib.import_module(filename)
+    except ImportError:
+        report_and_exit('No script named "{}".py found on current directory'.format(filename))
 
 
 def report_and_exit(msg):
@@ -20,11 +43,7 @@ def do(expid, steps=None, workspace=None, show_steps_only=False):
         report_and_exit('Wrong experiment ID "{}"'.format(expid))
 
     scriptname, expname = name_parts
-
-    try:
-        mod = importlib.import_module(scriptname)
-    except ImportError:
-        report_and_exit('No script named "{}".py found on current directory'.format(scriptname))
+    mod = import_experiment_file(scriptname)
 
     try:
         experiments = mod.experiments()
