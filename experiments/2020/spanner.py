b26c347a328b9244c1401a828521af13300b21cb
Guillem Frances <guillem.frances@gmail.com>
2020-07-09 10:18:30 +0200

    (Failing) Spanner experiment

--- experiments/2020/spanner.py
+++ experiments/2020/spanner.py
@@ -16,15 +16,17 @@ def experiments():
     exps["small"] = update_dict(
         base,
         pipeline="transition_classifier",
-        instances=[
-            "prob-6_4_10.pddl"
-        ],
+        instances=["prob-3-2-2-.pddl"],
         test_instances=[
             # "prob-10-10-10-1540903568.pddl"
         ],
-        num_states=20000,
-        max_concept_size=12,
-        concept_generation_timeout=60,  # in seconds
+        test_policy_instances=[
+            # "prob-10-10-10-1540903568.pddl"
+        ],
+        num_states="all",
+        max_concept_size=10,
+        distance_feature_max_complexity=8,
+        concept_generation_timeout=120,  # in seconds
         concept_generator=None,
         parameter_generator=None,
         maxsat_encoding="separation",
