d4613acd17584a98e69936265f73dc42db962307
Guillem Frances <guillem.frances@gmail.com>
2020-09-07 17:43:57 +0200

    Some preliminary attempts at floortile

---
+++ experiments/2020/floortile.py
@@ -0,0 +1,42 @@
+from sltp.util.misc import update_dict
+from sltp.util.names import floortile_names
+
+
+def experiments():
+    base = dict(
+        domain_dir="floortile-opt11-strips",
+        domain="domain.pddl",
+        test_domain="domain.pddl",
+        feature_namer=floortile_names,
+        pipeline="transition_classifier",
+        maxsat_encoding="separation",
+        complete_only_wrt_optimal=True,
+        prune_redundant_states=False,
+        optimal_selection_strategy="complete",
+        num_states="all",
+        concept_generator=None,
+        parameter_generator=None,
+        v_slack=2,
+
+        # concept_generation_timeout=120,  # in seconds
+        maxsat_timeout=None,
+    )
+
+    exps = dict()
+
+    exps["small"] = update_dict(
+        base,
+        instances=["training1.pddl"],
+        # instances=["testing.pddl"],
+        test_instances=[],
+        test_policy_instances=["opt-p01-002.pddl"],
+
+        max_concept_size=8,
+        # parameter_generator=gripper_parameters,  # Works also, but no real advantage
+        parameter_generator=None,
+        use_equivalence_classes=True,
+        # use_feature_dominance=True,
+        # use_incremental_refinement=True,
+    )
+
+    return exps
