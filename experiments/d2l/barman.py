4f3de897869c46ad5e93b40b60ea3bef57058759
Guillem Frances <guillem.frances@gmail.com>
2020-10-31 11:28:04 +0100

    Unsuccessful attempt at barman

---
+++ experiments/d2l/barman.py
@@ -0,0 +1,49 @@
+from sltp.util.misc import update_dict
+from sltp.util.names import barman_names
+
+
+def experiments():
+    base = dict(
+        domain_dir="barman-opt11-strips",
+        domain="domain.pddl",
+        test_domain="domain.pddl",
+        feature_namer=barman_names,
+        pipeline="transition_classifier",
+        maxsat_encoding="separation",
+        complete_only_wrt_optimal=True,
+        prune_redundant_states=False,
+        optimal_selection_strategy="complete",
+        num_states="all",
+        concept_generator=None,
+        parameter_generator=None,
+        v_slack=2,
+
+        # concept_generation_timeout=120,  # in seconds
+        maxsat_timeout=None,
+    )
+
+    exps = dict()
+
+    exps["small"] = update_dict(
+        base,
+        instances=[
+            'sample01.pddl',
+        ],
+        test_instances=[
+            # 'child-snack_pfile01-2.pddl',
+        ],
+        test_policy_instances=all_test_instances(),
+
+        max_concept_size=8,
+        distance_feature_max_complexity=8,
+
+        use_equivalence_classes=True,
+        # use_feature_dominance=True,
+        use_incremental_refinement=True,
+    )
+
+    return exps
+
+
+def all_test_instances():
+    return ['pfile01-001.pddl']
